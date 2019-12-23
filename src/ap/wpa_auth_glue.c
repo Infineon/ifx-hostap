17d64099a48c32e480c72f553215f7dbba30b753
Chung-Hsien Hsu <stanley.hsu@cypress.com>
2019-12-22 20:21:54 -0600

    P2P: Fix P2P authentication failure due to AP-mode 4-way handshake offload
    Commit 6df8c5e8669d ("AP: Support 4-way handshake offload for
    WPA/WPA2-PSK") offloaded the 4-way handshake in AP mode. P2P GO may not
    support it so do not set the offload indication for the case.
    
    Signed-off-by: Chung-Hsien Hsu <stanley.hsu@cypress.com>
    
    Jira: SWLINUX-1318

--- src/ap/wpa_auth_glue.c
+++ src/ap/wpa_auth_glue.c
@@ -1305,7 +1305,8 @@ int hostapd_setup_wpa(struct hostapd_data *hapd)
 		_conf.tx_status = 1;
 	if (hapd->iface->drv_flags & WPA_DRIVER_FLAGS_AP_MLME)
 		_conf.ap_mlme = 1;
-	if (hapd->iface->drv_flags & WPA_DRIVER_FLAGS_4WAY_HANDSHAKE_AP_PSK)
+	if (!hapd->conf->p2p &&
+	    (hapd->iface->drv_flags & WPA_DRIVER_FLAGS_4WAY_HANDSHAKE_AP_PSK))
 		_conf.psk_4way_hs_offload = 1;
 #ifdef CONFIG_SAE
 	if (hapd->iface->drv_flags & WPA_DRIVER_FLAGS_SAE_OFFLOAD)
